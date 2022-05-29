4868ee697e4055c54c2577453fb19f8e852fa948
Delegado <maxharper2013@gmail.com>
2022-05-29 14:52:09 -0300

    [ADD] componente categorias

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
+import { App } from './App'
 import { createRoot } from 'react-dom/client'
 const container = document.getElementById('app')
 const root = createRoot(container) // createRoot(container!) if you use TypeScript
-root.render(<h1>"Hola Platzi"</h1>)
+root.render(<App />)
