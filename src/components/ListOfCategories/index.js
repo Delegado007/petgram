1aea1abbc7fd7d83f9cc18d46f50a960f0aa4659
Delegado <maxharper2013@gmail.com>
2022-05-29 22:35:06 -0300

    [ADD] lista de categorias flotante

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -4,6 +4,7 @@ import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
 
   useEffect(() => {
     window.fetch('https://api-petgram-875dq86v1-delegado007.vercel.app/categories')
@@ -13,11 +14,32 @@ export const ListOfCategories = () => {
       })
   }, [])
 
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => {
+    return (
+      <List className={fixed ? 'fixed' : ''}>
+        {
+          categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+        }
+      </List>
+
+    )
+  }
+
   return (
-    <List>
-      {
-        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
-      }
-    </List>
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
+
   )
 }
