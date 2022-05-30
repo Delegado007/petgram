c6ea8950a90701506e023e242269b8cd837acecd
Delegado <maxharper2013@gmail.com>
2022-05-29 23:31:37 -0300

    [ADD] polifier que permite usar intersection observer en todos los navegadores

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -8,15 +8,20 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, setShow] = useState(false)
 
   useEffect(function () {
-    const observer = new window.IntersectionObserver(function (entries) {
-      const { isIntersecting } = entries[0]
-      if (isIntersecting) {
-        console.log('Si')
-        setShow(true)
-        observer.disconnect()
-      }
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined'
+        ? window.IntersectionObserver
+        : import('intersection-observer')
+    ).then(() => {
+      const observer = new window.IntersectionObserver(function (entries) {
+        const { isIntersecting } = entries[0]
+        if (isIntersecting) {
+          setShow(true)
+          observer.disconnect()
+        }
+      })
+      observer.observe(element.current)
     })
-    observer.observe(element.current)
   }, [element])
 
   return (
@@ -32,9 +37,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
             <MdFavoriteBorder size='32px' /> {likes} likes!
           </Button>
         </>
-
       }
-
     </Article>
   )
 }
