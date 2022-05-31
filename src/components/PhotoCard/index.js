5c327bcaa0e7ab7884cbf53b571961e8e2823d71
Delegado <maxharper2013@gmail.com>
2022-05-31 01:12:14 -0300

    [ADD] graphQL gql

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { ImgWrapper, Img, Button, Article } from './styles'
+
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+
 import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearSreen } from '../../hooks/useNearScreen'
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+
+
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearSreen()
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import { App } from './App'
 import { createRoot } from 'react-dom/client'
+
+const client = new ApolloClient({
+  uri: 'https://api-petgram-875dq86v1-delegado007.vercel.app/graphql',
+  cache: new InMemoryCache()
+})
+
 const container = document.getElementById('app')
 const root = createRoot(container) // createRoot(container!) if you use TypeScript
-root.render(<App />)
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
