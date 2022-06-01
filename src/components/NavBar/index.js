64bd80a082e2bf781cf01ce6cfb4139fe136bf43
Delegado <maxharper2013@gmail.com>
2022-06-01 02:34:24 -0300

    [ADD] navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React from 'react';
+import { Nav, Link } from './styles'
+import { MdHome, MdFavoriteBorder, MdPersonOutline } from 'react-icons/md'
 
 export const NavBar = () => {
+  const SIZE = '32px'
   return (
-    <nav>
-      <button>home</button>
-      <button>fav</button>
-      <button>user</button>
-    </nav>
+    <Nav>
+      <Link to='/'><MdHome size={SIZE} /></Link>
+      <Link to='/favs'><MdFavoriteBorder size={SIZE} /></Link>
+      <Link to='/user'><MdPersonOutline size={SIZE} /></Link>
+    </Nav>
   )
 }
