0b213c3ffc8c25211e8f33c7d673a0721be18e1f
Delegado <maxharper2013@gmail.com>
2022-06-01 03:00:30 -0300

    [ADD] rutas protegidas

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import { Home } from './pages/Home'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Detail } from './pages/Detail';
 import { NavBar } from './components/NavBar';
+import { Favs } from './pages/Favs';
+import { User } from './pages/User';
+import { NotRegisteredUser } from './pages/NotRegisteredUser';
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: true })
+}
 
 export const App = () => {
   return (
@@ -16,6 +23,20 @@ export const App = () => {
         <Route path='/pet/:id' element={<Home />} />
         <Route path='/detail/:detailId' element={<Detail />} />
       </Routes>
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? <Routes>
+                <Route path='/favs' element={<Favs />} />
+                <Route path='/user' element={<User />} />
+              </Routes>
+              : <Routes>
+                <Route path='/favs' element={<NotRegisteredUser />} />
+                <Route path='/user' element={<NotRegisteredUser />} />
+              </Routes>
+        }
+      </UserLogged>
       <NavBar />
     </BrowserRouter>
   )
